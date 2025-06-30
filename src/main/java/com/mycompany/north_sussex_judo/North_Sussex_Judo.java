2360b29c8299787aa86ea963a5a14bfc6095b187
Rehan <65840084+Rehan20021104@users.noreply.github.com>
2025-06-30 21:21:18 +0530

    Add files via upload

---
+++ src/main/java/com/mycompany/north_sussex_judo/North_Sussex_Judo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.north_sussex_judo;
+
+/**
+ *
+ * @author RR
+ */
+public class North_Sussex_Judo {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
